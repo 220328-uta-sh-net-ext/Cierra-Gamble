1c62f43163ebe19e623a7b65fc4a15a096b56e83
G3mtech <100974782+G3mtech@users.noreply.github.com>
2022-04-13 14:34:45 -0400

    Add files via upload

---
+++ Hello World DotNet.cs
@@ -0,0 +1,19 @@
+//for starters, when you start on this terminal and typing is disabled, press esc + i for 'insert"
+// to open folders with spaces in them, use backslash before each name (\  _____). ex; ...Cierra-Gamble\  Hello\ World 
+// following exercise is Hello World using .Net
+// Console.WriteLINE is to write one line
+// Console.Write is to write MULTIPLE lines
+// To print to the output console, but at the end, not adding a line feed, Console.Write
+
+
+
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is the second line.");
+
+Console.WriteLine("Hello World");
+
+Console.Write("Congratulations Cierra!");
+Console.WriteLine("Congratulations!");
+
+
